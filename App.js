2417f915431184b9be8329ec40be9303b90cb34d
Rhys Brame <rhysbrame@rhyss-mbp.home>
2019-01-04 20:13:15 +0000

    Adding Navigator and fixing known RN bugs

--- App.js
+++ App.js
@@ -7,46 +7,27 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import HomeView from "./Components/HomeView";
+import TeamsView from "./Components/TeamsView";
 
 type Props = {};
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeView,
+    Teams: TeamsView
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
 export default class App extends Component<Props> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to NFL Squad Viewer with React Native!
-        </Text>
-        <Text style={styles.welcome}>Get Tae!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
